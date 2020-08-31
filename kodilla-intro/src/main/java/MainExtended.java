c082dff683a0519aec472ac0f3bb243a455989e2
Gosia Jakiel <gwyn.morlo@gmail.com>
2020-08-31 21:52:09 +0200

    Zmienne

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,17 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String myName = "Gosia";
+        int myAge = 30;
+        String favMusicGenre = "Rock";
+        double catsOwned = 1;
+        String myFavGame = "League of Legends";
+        boolean isARedhead = true;
+
+        System.out.println(myName);
+        System.out.println(myAge);
+        System.out.println(favMusicGenre);
+        System.out.println(catsOwned);
+        System.out.println(myFavGame);
+        System.out.println(isARedhead);
+    }
+}
