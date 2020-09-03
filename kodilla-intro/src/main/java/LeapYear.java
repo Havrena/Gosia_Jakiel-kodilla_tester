0ad4f437e18985c020abd9605718e282a3bb2869
Gosia Jakiel <gwyn.morlo@gmail.com>
2020-09-03 14:06:20 +0200

    Leap Year

--- kodilla-intro/src/main/java/LeapYear.java
+++ kodilla-intro/src/main/java/LeapYear.java
@@ -1,2 +1,10 @@
 public class LeapYear {
+    public static void main(String[] args) {
+        int year = 2020;
+
+        if((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0))
+            System.out.println(year + " to rok przestępny.");
+        else
+            System.out.println(year + " nie jest rokiem przestępnym.");
+    }
 }
