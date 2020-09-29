e473659b89b4b208c458928d0fd87b68eb56629c
Gosia Jakiel <gwyn.morlo@gmail.com>
2020-09-29 20:16:58 +0200

    Debug

--- kodilla-intro/src/main/java/FirstClass.java
+++ kodilla-intro/src/main/java/FirstClass.java
@@ -2,22 +2,19 @@ public class FirstClass {
     public static void main(String[] args) {
 
         Notebook notebook = new Notebook(600, 1000, 2019);
+        getData(notebook);
+
+        Notebook heavyNotebook = new Notebook(2000, 1500, 2017);
+        getData(heavyNotebook);
+
+        Notebook oldNotebook = new Notebook(1600, 500, 2015);
+        getData(oldNotebook);
+    }
+
+    public static void getData(Notebook notebook) {
         System.out.println(notebook.weight + "g " + notebook.price + "$ " + notebook.year);
         notebook.checkPrice();
         notebook.checkWeight();
         notebook.checkYearAndPrice();
-
-        Notebook heavyNotebook = new Notebook(2000, 1500, 2017);
-        System.out.println(heavyNotebook.weight + "g " + heavyNotebook.price + "$ " + heavyNotebook.year);
-        heavyNotebook.checkPrice();
-        heavyNotebook.checkWeight();
-        heavyNotebook.checkYearAndPrice();
-
-        Notebook oldNotebook = new Notebook(1600, 500, 2015);
-        System.out.println(oldNotebook.weight + "g " + oldNotebook.price + "$ " + oldNotebook.year);
-        oldNotebook.checkPrice();
-        oldNotebook.checkWeight();
-        oldNotebook.checkYearAndPrice();
-
     }
 }
